a017d602580e1f3ebad44ecacb270c689b5e97eb
Fabricio Catae <fcatae@microsoft.com>
2018-06-22 20:54:11 -0300

    serialize the stack trace

--- src/Workflow.cs
+++ src/Workflow.cs
@@ -3,82 +3,103 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using System.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace TaskFlow
 {
-    class WorkflowImpl1 : Workflow
-    {
-        class ScopeVar1 : ScopeVariables
-        {
-            public int i;
-        }
-
-        public async Task Contagem(int i)
-        {
-            using (var scope = await ScopeAsync<ScopeVar1>("contagem"))
-            {
-                Bot bot = scope.Use<Bot>();
-
-                Code("init", () =>
-                {
-                    bot.Send("Contagem2");
-                    i = i + 1;
-                });
-
-                Code("list", () =>
-                {
-                    for (; i < 10; i++)
-                    {
-                        bot.Send("Hello " + i);
-                    }
-                });
-
-                Code("list2", () =>
-                {
-                    Code("list2-botinit", () => { i = 0; });
-
-                    while(i < 10)
-                    {
-                        Code("list2-botsend", () =>
-                        {
-                            bot.Send("Hello " + i);
-                            i++;
-                        });
-                    }
-                });
-
-                Code("user", () =>
-                {
-                    string digi = bot.Recv();
-
-                    while (digi != "1")
-                    {
-                        bot.Send("Type 1");
-                        digi = bot.Recv();
-                    }
-                });
-            }
-        }
-    }
-
     abstract class Workflow
     {
+        WorkspaceScopeList _scopes = new WorkspaceScopeList();
+
         protected Task<T> ScopeAsync<T>(string name)
-            where T: IDisposable, new()
+            where T: ScopeVariables, new()
         {
-            return Task.FromResult(new T());
+            var scope = new T();
+            _scopes.Add(new WorkspaceScope { Name = name, TypeName = typeof(T).FullName, Variables = scope });
+
+            return Task.FromResult(scope);
         }
 
         protected void Code(string name, Action action)
         {
-            Console.WriteLine("Step: " + name);
+            RetrieveScopeInformation(name);            
             action();
         }
+
+        private void RetrieveScopeInformation(string name)
+        {
+            string text = WorkspaceScopeList.Serialize(_scopes);
+
+            string msg = $"Step: {name} - {text}";
+
+            var list = WorkspaceScopeList.Deserialize(text);
+
+            Console.WriteLine(msg);  
+        }
+    }
+
+    class WorkspaceScopeList
+    {
+        List<WorkspaceScope> _scopes = new List<WorkspaceScope>();
+
+        public void Add(WorkspaceScope scope)
+        {
+            _scopes.Add(scope);
+        }
+
+        public static string Serialize(WorkspaceScopeList workspaceScopeList)
+        {
+            return JsonConvert.SerializeObject(workspaceScopeList._scopes);
+        }
+
+        public static WorkspaceScopeList Deserialize(string text)
+        {
+            var list = JsonConvert.DeserializeObject<List<WorkspaceScopeGeneric>>(text);
+
+            var scopeList = new WorkspaceScopeList();
+
+            foreach (var line in list)
+            {
+                var variableType = Type.GetType(line.TypeName);
+                var variables = line.Variables.ToObject(variableType);
+
+                WorkspaceScope scope = new WorkspaceScope()
+                {
+                    Name = line.Name,
+                    TypeName = line.TypeName,
+                    Variables = (ScopeVariables)variables
+                };
+
+                scopeList.Add(scope);
+            }
+            return scopeList;
+        }
+
+        class WorkspaceScopeGeneric
+        {
+            public string Name { get; set; }
+            public string TypeName { get; set; }
+            public JObject Variables { get; set; }
+        }
+    }
+
+    class WorkspaceScope
+    {
+        public string Name { get; set; }
+        public string TypeName { get; set; }
+        public ScopeVariables Variables { get; set; }
     }
 
     abstract class ScopeVariables : IDisposable
     {
-        Dictionary<Type,object> _instances = new Dictionary<Type, object>;
+        Dictionary<Type,object> _instances = new Dictionary<Type, object>();
+        
+        public bool IsActive()
+        {
+            return (_instances != null);
+        }
 
         public T Use<T>()
             where T: new()
@@ -104,7 +125,7 @@ namespace TaskFlow
             foreach(var instance in _instances.Values)
             {
                 var disposable = instance as IDisposable;
-                if(instance != null)
+                if(disposable != null)
                 {
                     disposable.Dispose();
                 }
---
+++ src/WorkflowImpl1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskFlow
+{
+    class ScopeVar1 : ScopeVariables
+    {
+        public int i;
+    }
+
+    class WorkflowImpl1 : Workflow
+    {
+
+        public async Task Contagem(int i)
+        {
+            using (var scope = await ScopeAsync<ScopeVar1>("contagem"))
+            {
+                Bot bot = scope.Use<Bot>();
+
+                Code("init", () =>
+                {
+                    scope.i = i;
+
+                    bot.Send("Contagem2");
+                    scope.i = scope.i + 1;
+                });
+
+                Code("list", () =>
+                {
+                    for (; scope.i < 10; scope.i++)
+                    {
+                        bot.Send("Hello " + scope.i);
+                    }
+                });
+
+                Code("list2", () =>
+                {
+                    Code("list2-botinit", () => { scope.i = i; });
+
+                    while (scope.i < 10)
+                    {
+                        Code("list2-botsend", () =>
+                        {
+                            bot.Send("Hello " + scope.i);
+                            scope.i++;
+                        });
+                    }
+                });
+
+                Code("user", () =>
+                {
+                    string digi = bot.Recv();
+
+                    while (digi != "1")
+                    {
+                        bot.Send("Type 1");
+                        digi = bot.Recv();
+                    }
+                });
+            }
+        }
+    }
+}
